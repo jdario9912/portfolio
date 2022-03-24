05f7d88409ac7f85a033c4954da27ae4587be21d
Joel Dario <joeldario9912@gmail.com>
2022-03-24 15:37:58 -0300

    agregue funcionalidad al boton hamburguesa

---
+++ assets/script/funciones.js
@@ -0,0 +1,3 @@
+function ocultarElemento(elemento) {
+    elemento.classList.add('ocultar-elemento');
+}
--- assets/script/ocultar-nav.js
+++ assets/script/ocultar-nav.js
@@ -1,24 +1,24 @@
-const hamburguesa = document.querySelector('#hamburguesa');
+const btnOcultarNav = document.querySelector('#btn-ocultar-nav');
 
-hamburguesa.addEventListener('click', function (event) {
+btnOcultarNav.addEventListener('click', function (event) {
     event.preventDefault();
     const nav = document.querySelector('#menu__nav');
     const navLista = document.querySelector('#menu__lista');
     const navItem = document.querySelectorAll('#menu__item');
     const navLink = document.querySelectorAll('#menu__link');
     const btnOcultarNav = document.querySelector('#btn-ocultar-nav');
-    nav.classList.remove('ocultar-elemento');
-    btnOcultarNav.classList.remove('btn-ocultar-nav');
-    navLista.classList.add('lista-movile');
-    nav.classList.add('nav-movile');
-    btnOcultarNav.classList.add('btn-ocultar-movile');
+    nav.classList.remove('nav-movile');
+    navLista.classList.remove('lista-movile');
     
     for (let i = 0; i < navItem.length; i++) {
-        navItem[i].classList.add('item-movile');
+        navItem[i].classList.remove('item-movile');
         
     }
     
     for (let i = 0; i < navLink.length; i++) {
-        navLink[i].classList.add('link-movile');
+        navLink[i].classList.remove('link-movile');
     }
+
+    btnOcultarNav.classList.remove('btn-ocultar-movile');
+    ocultarElemento(btnOcultarNav);
 });
