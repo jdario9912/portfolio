bc22057b8e04f47f5dee6ad2bbd2c02b9ef03f19
Joel Dario <joeldario9912@gmail.com>
2022-03-24 02:03:43 -0300

    probe aparicion de nav

--- assets/script/desplegar-nav.js
+++ assets/script/desplegar-nav.js
@@ -3,4 +3,19 @@ const hamburguesa = document.querySelector('.hamburguesa-container');
 hamburguesa.addEventListener('click', function (event) {
     event.preventDefault();
     console.log('aprete la hamburguesa');
-})
+    const nav = document.querySelector('#menu__nav');
+    const navLista = document.querySelector('#menu__lista');
+    const navItem = document.querySelectorAll('#menu__item');
+    const navLink = document.querySelectorAll('#menu__link');
+    navLista.classList.add('lista-vertical');
+    nav.classList.add('desplegar-nav');
+    
+    for (let i = 0; i < navItem.length; i++) {
+        navItem[i].classList.add('item-total');
+        
+    }
+
+    for (let i = 0; i < navLink.length; i++) {
+        navLink[i].classList.add('link-rojo');
+    }
+});
