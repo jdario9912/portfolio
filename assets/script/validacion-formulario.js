f311abd013a919619586b676744987a49656ba2d
Joel Dario <joeldario9912@gmail.com>
2022-03-30 20:57:10 -0300

    modifique la url de linkedin

--- assets/script/validacion-formulario.js
+++ assets/script/validacion-formulario.js
@@ -1,34 +1,44 @@
-const nombreForm = document.querySelector('[data-nombre]');
-const emailForm = document.querySelector('[data-email]');
-const asuntoForm = document.querySelector('[data-asunto]');
-const mensajeForm = document.querySelector('[data-mensaje]');
+const nombreInput = document.querySelector('[data-nombre]');
+const emailInput = document.querySelector('[data-email]');
+const asuntoInput = document.querySelector('[data-asunto]');
+const mensajeInput = document.querySelector('[data-mensaje]');
 
 const alertNombreContainer = document.querySelector('[data-alert-nombre-container]');
 const alertEmailContainer = document.querySelector('[data-alert-email-container]');
 const alertAsuntoContainer = document.querySelector('[data-alert-asunto-container]');
 const alertMensajeContainer = document.querySelector('[data-alert-mensaje-container]');
 
-nombreForm.value = '';
-emailForm.value = '';
-asuntoForm.value = '';
-mensajeForm.value = '';
+nombreInput.value = '';
+emailInput.value = '';
+asuntoInput.value = '';
+mensajeInput.value = '';
 
-const nombreValidado = false;
-const emailValidado = false;
-const asuntoValidado = false;
-const mensajeValidado = false;
+// const nombreValidado = false;
+// const emailValidado = false;
+// const asuntoValidado = false;
+// const mensajeValidado = false;
 
-nombreForm.addEventListener('input', function(){
-    let span
-    if (nombreForm.value == '') {
-        span = document.createElement('span');
-        span.textContent = '*este campo es obligatorio';
-        styleAlert(span);
-        alertNombreContainer.appendChild(span);
-        nombreValidado = false;
+nombreInput.addEventListener('blur', (evento) => {
+    let alerta;
+    nombre = evento.target.value;
+    if (nombre == '') {
+        alerta = document.createElement('span');
+        alerta.textContent = '*este campo es obligatorio';
+        alertNombreContainer.appendChild(alerta);
     }else{
-        alertNombreContainer.removeChild(span);
-        nombreValidado = true;
-    }   
-    return nombreValidado
+        alertNombreContainer.removeChild(alerta);
+        console.log(`el nombre del usuario es ${nombre}`);
+    }
+});
+
+emailInput.addEventListener('blur', () =>{
+    console.log('Campo email');
+});
+
+asuntoInput.addEventListener('blur', () =>{
+    console.log('Campo asunto');
+});
+
+mensajeInput.addEventListener('blur', () =>{
+    console.log('Campo mensaje');
 });
